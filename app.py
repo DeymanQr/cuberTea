196f8b41f45d0185e9c1641fa1b827c63c15d0f7
GuyFawkess <skakunostap@gmail.com>
2020-05-16 09:49:15 +0300

    Add requirements and change app

--- app.py
+++ app.py
@@ -132,4 +132,5 @@ def shop():
         return render_template("shop.html", data=find_product(product))
     return render_template("shop.html")
 
-app.run(port=5001, debug=True)
+if __name__ == '__main__':
+    app.run()
